5d0c5e9f279be6afdf30b963f972dda5e6351e57
ahamefunad1237 <140945618+ahamefunad1237@users.noreply.github.com>
2023-07-30 14:27:52 +0100

    Update Object.java

--- Object.java
+++ Object.java
@@ -4,5 +4,19 @@ public final class Object {
    public String mdata;
    public int mdatalength;
 
-  
+  public Object(String mdata,int mdatalength){
+     this.mdata = mdata;
+     this.mdatalength = mdatalength;
+
+     if(!mdata.equals("")){
+        mdatalength = mdata.length();
+     }
+  }
+
+   public int getDataString(){
+      if(mdatalength > 0){
+         return mdatalength
+      }
+      return 0
+   }
 }
